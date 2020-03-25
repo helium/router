46f6aabcdfcaa5084c4467c03550383bbd2c1207
macpie <pierre@helium.com>
2020-03-25 16:47:02 -0700

    Add test for http downlink

--- test/router_channel_http_SUITE.erl
+++ test/router_channel_http_SUITE.erl
@@ -49,6 +49,130 @@ end_per_testcase(TestCase, Config) ->
 %%--------------------------------------------------------------------
 
 http_test(Config) ->
+    Tab = proplists:get_value(ets, Config),
+    AppKey = proplists:get_value(app_key, Config),
+    Swarm = proplists:get_value(swarm, Config),
+    {ok, RouterSwarm} = router_p2p:swarm(),
+    [Address|_] = libp2p_swarm:listen_addrs(RouterSwarm),
+    {ok, Stream} = libp2p_swarm:dial_framed_stream(Swarm,
+                                                   Address,
+                                                   router_handler_test:version(),
+                                                   router_handler_test,
+                                                   [self()]),
+    PubKeyBin = libp2p_swarm:pubkey_bin(Swarm),
+    {ok, HotspotName} = erl_angry_purple_tiger:animal_name(libp2p_crypto:bin_to_b58(PubKeyBin)),
+
+    %% Send join packet
+    JoinNonce = crypto:strong_rand_bytes(2),
+    Stream ! {send, test_utils:join_packet(PubKeyBin, AppKey, JoinNonce)},
+    timer:sleep(?JOIN_DELAY),
+
+    %% Waiting for report device status on that join request
+    test_utils:wait_report_device_status(#{<<"status">> => <<"success">>,
+                                           <<"description">> => '_',
+                                           <<"reported_at">> => fun erlang:is_integer/1,
+                                           <<"category">> => <<"activation">>,
+                                           <<"frame_up">> => 0,
+                                           <<"frame_down">> => 0,
+                                           <<"hotspot_name">> => erlang:list_to_binary(HotspotName)}),
+    %% Waiting for reply from router to hotspot
+    test_utils:wait_state_channel_message(1250),
+
+    %% Check that device is in cache now
+    {ok, DB, [_, CF]} = router_db:get(),
+    WorkerID = router_devices_sup:id(?CONSOLE_DEVICE_ID),
+    {ok, Device0} = router_device:get(DB, CF, WorkerID),
+
+    %% Send UNCONFIRMED_UP frame packet
+    DownlinkPayload = <<"httpdownlink">>,
+    ets:insert(Tab, {http_resp, jsx:encode(#{payload_raw => base64:encode(DownlinkPayload)})}),
+    Stream ! {send, test_utils:frame_packet(?UNCONFIRMED_UP, PubKeyBin, router_device:nwk_s_key(Device0),
+                                            router_device:app_s_key(Device0), 0)},
+
+    %% Waiting for data from HTTP channel
+    test_utils:wait_channel_data(#{<<"id">> => ?CONSOLE_DEVICE_ID,
+                                   <<"name">> => ?CONSOLE_DEVICE_NAME,
+                                   <<"dev_eui">> => lorawan_utils:binary_to_hex(?DEVEUI),
+                                   <<"app_eui">> => lorawan_utils:binary_to_hex(?APPEUI),
+                                   <<"metadata">> => #{<<"labels">> => ?CONSOLE_LABELS},
+                                   <<"sequence">> => 0,
+                                   <<"timestamp">> => fun erlang:is_integer/1,
+                                   <<"payload">> => <<>>,
+                                   <<"port">> => 1,
+                                   <<"hotspots">> => [#{<<"name">> => erlang:list_to_binary(HotspotName),
+                                                        <<"timestamp">> => 0,
+                                                        <<"rssi">> => 0.0,
+                                                        <<"snr">> => 0.0,
+                                                        <<"spreading">> => <<"SF8BW125">>}]}),
+
+    %% Waiting for report channel status from HTTP channel
+    test_utils:wait_report_channel_status(#{<<"status">> => <<"success">>,
+                                            <<"description">> => '_',
+                                            <<"channel_id">> => ?CONSOLE_HTTP_CHANNEL_ID,
+                                            <<"channel_name">> => ?CONSOLE_HTTP_CHANNEL_NAME,
+                                            <<"reported_at">> => fun erlang:is_integer/1,
+                                            <<"category">> => <<"up">>,
+                                            <<"frame_up">> => fun erlang:is_integer/1,
+                                            <<"frame_down">> => fun erlang:is_integer/1,
+                                            <<"payload_size">> => 0,
+                                            <<"payload">> => <<>>,
+                                            <<"hotspots">> => [#{<<"name">> => erlang:list_to_binary(HotspotName),
+                                                                 <<"timestamp">> => 0,
+                                                                 <<"rssi">> => 0.0,
+                                                                 <<"snr">> => 0.0,
+                                                                 <<"spreading">> => <<"SF8BW125">>}]}),
+
+    %% We ignore the channel correction  and down messages
+    ok = test_utils:ignore_messages(),
+
+    %% As HTTP channel is async we added a downlink msg in Q with the last uplink triggering another 
+    %% uplink should flush that Q and give us a dwonlink message this time
+    Stream ! {send, test_utils:frame_packet(?UNCONFIRMED_UP, PubKeyBin, router_device:nwk_s_key(Device0),
+                                            router_device:app_s_key(Device0), 1)},
+
+    %% Waiting for data from HTTP channel
+    test_utils:wait_channel_data(#{<<"id">> => ?CONSOLE_DEVICE_ID,
+                                   <<"name">> => ?CONSOLE_DEVICE_NAME,
+                                   <<"dev_eui">> => lorawan_utils:binary_to_hex(?DEVEUI),
+                                   <<"app_eui">> => lorawan_utils:binary_to_hex(?APPEUI),
+                                   <<"metadata">> => #{<<"labels">> => ?CONSOLE_LABELS},
+                                   <<"sequence">> => 1,
+                                   <<"timestamp">> => fun erlang:is_integer/1,
+                                   <<"payload">> => <<>>,
+                                   <<"port">> => 1,
+                                   <<"hotspots">> => [#{<<"name">> => erlang:list_to_binary(HotspotName),
+                                                        <<"timestamp">> => 0,
+                                                        <<"rssi">> => 0.0,
+                                                        <<"snr">> => 0.0,
+                                                        <<"spreading">> => <<"SF8BW125">>}]}),
+
+    %% Waiting for report channel status from HTTP channel
+    test_utils:wait_report_channel_status(#{<<"status">> => <<"success">>,
+                                            <<"description">> => '_',
+                                            <<"channel_id">> => ?CONSOLE_HTTP_CHANNEL_ID,
+                                            <<"channel_name">> => ?CONSOLE_HTTP_CHANNEL_NAME,
+                                            <<"reported_at">> => fun erlang:is_integer/1,
+                                            <<"category">> => <<"up">>,
+                                            <<"frame_up">> => fun erlang:is_integer/1,
+                                            <<"frame_down">> => fun erlang:is_integer/1,
+                                            <<"payload_size">> => 0,
+                                            <<"payload">> => <<>>,
+                                            <<"hotspots">> => [#{<<"name">> => erlang:list_to_binary(HotspotName),
+                                                                 <<"timestamp">> => 0,
+                                                                 <<"rssi">> => 0.0,
+                                                                 <<"snr">> => 0.0,
+                                                                 <<"spreading">> => <<"SF8BW125">>}]}),
+
+    %% Waiting for donwlink message on the hotspot
+    Msg0 = {false, 1, DownlinkPayload},
+    {ok, _} = test_utils:wait_state_channel_message(Msg0, Device0, erlang:element(3, Msg0), ?UNCONFIRMED_DOWN, 0, 0, 1, 1),
+
+    %% Ignore down messages updates
+    ok = test_utils:ignore_messages(),
+    
+    ok.
+
+http_update_test(Config) ->
     AppKey = proplists:get_value(app_key, Config),
     Swarm = proplists:get_value(swarm, Config),
     {ok, RouterSwarm} = router_p2p:swarm(),
@@ -117,77 +241,9 @@ http_test(Config) ->
                                                                  <<"rssi">> => 0.0,
                                                                  <<"snr">> => 0.0,
                                                                  <<"spreading">> => <<"SF8BW125">>}]}),
-    ok.
 
-http_update_test(Config) ->
-    AppKey = proplists:get_value(app_key, Config),
-    Swarm = proplists:get_value(swarm, Config),
-    {ok, RouterSwarm} = router_p2p:swarm(),
-    [Address|_] = libp2p_swarm:listen_addrs(RouterSwarm),
-    {ok, Stream} = libp2p_swarm:dial_framed_stream(Swarm,
-                                                   Address,
-                                                   router_handler_test:version(),
-                                                   router_handler_test,
-                                                   [self()]),
-    PubKeyBin = libp2p_swarm:pubkey_bin(Swarm),
-    {ok, HotspotName} = erl_angry_purple_tiger:animal_name(libp2p_crypto:bin_to_b58(PubKeyBin)),
-
-    %% Send join packet
-    JoinNonce = crypto:strong_rand_bytes(2),
-    Stream ! {send, test_utils:join_packet(PubKeyBin, AppKey, JoinNonce)},
-    timer:sleep(?JOIN_DELAY),
-
-     %% Waiting for report device status on that join request
-    test_utils:wait_report_device_status(#{<<"status">> => <<"success">>,
-                                           <<"description">> => '_',
-                                           <<"reported_at">> => fun erlang:is_integer/1,
-                                           <<"category">> => <<"activation">>,
-                                           <<"frame_up">> => 0,
-                                           <<"frame_down">> => 0,
-                                           <<"hotspot_name">> => erlang:list_to_binary(HotspotName)}),
-    %% Waiting for reply from router to hotspot
-    test_utils:wait_state_channel_message(1250),
-
-    %% Check that device is in cache now
-    {ok, DB, [_, CF]} = router_db:get(),
-    WorkerID = router_devices_sup:id(?CONSOLE_DEVICE_ID),
-    {ok, Device0} = router_device:get(DB, CF, WorkerID),
-
-    %% Send UNCONFIRMED_UP frame packet
-    Stream ! {send, test_utils:frame_packet(?UNCONFIRMED_UP, PubKeyBin, router_device:nwk_s_key(Device0), router_device:app_s_key(Device0), 0)},
-
-    %% Waiting for data from HTTP channel
-    test_utils:wait_channel_data(#{<<"id">> => ?CONSOLE_DEVICE_ID,
-                                   <<"name">> => ?CONSOLE_DEVICE_NAME,
-                                   <<"dev_eui">> => lorawan_utils:binary_to_hex(?DEVEUI),
-                                   <<"app_eui">> => lorawan_utils:binary_to_hex(?APPEUI),
-                                   <<"metadata">> => #{<<"labels">> => ?CONSOLE_LABELS},
-                                   <<"sequence">> => 0,
-                                   <<"timestamp">> => fun erlang:is_integer/1,
-                                   <<"payload">> => <<>>,
-                                   <<"port">> => 1,
-                                   <<"hotspots">> => [#{<<"name">> => erlang:list_to_binary(HotspotName),
-                                                        <<"timestamp">> => 0,
-                                                        <<"rssi">> => 0.0,
-                                                        <<"snr">> => 0.0,
-                                                        <<"spreading">> => <<"SF8BW125">>}]}),
-
-    %% Waiting for report channel status from HTTP channel
-    test_utils:wait_report_channel_status(#{<<"status">> => <<"success">>,
-                                            <<"description">> => <<"success">>,
-                                            <<"channel_id">> => ?CONSOLE_HTTP_CHANNEL_ID,
-                                            <<"channel_name">> => ?CONSOLE_HTTP_CHANNEL_NAME,
-                                            <<"reported_at">> => fun erlang:is_integer/1,
-                                            <<"category">> => <<"up">>,
-                                            <<"frame_up">> => fun erlang:is_integer/1,
-                                            <<"frame_down">> => fun erlang:is_integer/1,
-                                            <<"payload_size">> => 0,
-                                            <<"payload">> => <<>>,
-                                            <<"hotspots">> => [#{<<"name">> => erlang:list_to_binary(HotspotName),
-                                                                 <<"timestamp">> => 0,
-                                                                 <<"rssi">> => 0.0,
-                                                                 <<"snr">> => 0.0,
-                                                                 <<"spreading">> => <<"SF8BW125">>}]}),
+    %% Ignore down messages updates
+    ok = test_utils:ignore_messages(),
 
     %% Change HTTP channel to PUT method (this will send back a 404 not found)
     Tab = proplists:get_value(ets, Config),
@@ -224,6 +280,9 @@ http_update_test(Config) ->
                                                                  <<"rssi">> => 0.0,
                                                                  <<"snr">> => 0.0,
                                                                  <<"spreading">> => <<"SF8BW125">>}]}),
+    %% Ignore down messages updates
+    ok = test_utils:ignore_messages(),
+
     ok.
 
 %% ------------------------------------------------------------------

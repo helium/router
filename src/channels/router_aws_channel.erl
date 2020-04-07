54b29ad8a6d197033936ec02d7a2dda857bfce50
macpie <pierre@helium.com>
2020-04-07 10:52:55 -0700

    Fix AWS channel status return

--- src/channels/router_aws_channel.erl
+++ src/channels/router_aws_channel.erl
@@ -52,8 +52,8 @@ init({[Channel, Device], _}) ->
 
 handle_event({data, Ref, Data}, #state{channel=Channel, connection=Conn, pubtopic=Topic}=State) ->
     Res = emqtt:publish(Conn, Topic, encode_data(Data), 0),
-    ok = handle_publish_res(Res, Channel, Ref, Data),
     lager:debug("published: ~p result: ~p", [Data, Res]),
+    ok = handle_publish_res(Res, Channel, Ref),
     {ok, State};
 handle_event(_Msg, State) ->
     lager:warning("rcvd unknown cast msg: ~p", [_Msg]),
@@ -96,18 +96,12 @@ ping(Conn) ->
 encode_data(#{payload := Payload}=Map) ->
     jsx:encode(maps:put(payload, base64:encode(Payload), Map)).
 
--spec handle_publish_res(any(), router_channel:channel(), reference(), map()) -> ok.
-handle_publish_res(Res, Channel, Ref, Data) ->
+-spec handle_publish_res(any(), router_channel:channel(), reference()) -> ok.
+handle_publish_res(Res, Channel, Ref) ->
     Pid = router_channel:controller(Channel),
-    Payload = maps:get(payload, Data),
-    Result0 = #{channel_id => router_channel:id(Channel),
-                channel_name => router_channel:name(Channel),
-                reported_at => erlang:system_time(seconds),
-                category => <<"up">>,
-                port => maps:get(port, Data),
-                payload => base64:encode(Payload),
-                payload_size => erlang:byte_size(Payload),
-                hotspots => maps:get(hotspots, Data)},
+    Result0 = #{id => router_channel:id(Channel),
+                name => router_channel:name(Channel),
+                reported_at => erlang:system_time(seconds)},
     Result1 = case Res of
                   {ok, PacketID} ->
                       maps:merge(Result0, #{status => success, description => list_to_binary(io_lib:format("Packet ID: ~b", [PacketID]))});

1dd349427af4d02c7b1552c6b536f32909f2c2ed
macpie <pierre@helium.com>
2020-04-16 11:42:39 -0700

    Improve test to include 10 uplink messages

--- test/router_device_api_console_SUITE.erl
+++ test/router_device_api_console_SUITE.erl
@@ -166,6 +166,76 @@ debug_test(Config) ->
     %% Making sure debug is on for our device and at 9 now
     State1 = sys:get_state(router_device_api_console),
     ?assertMatch({state, _, _, _, #{?CONSOLE_DEVICE_ID := 9}}, State1),
+
+    lists:foreach(
+      fun(I) ->
+              Stream ! {send, test_utils:frame_packet(?UNCONFIRMED_UP, PubKeyBin, router_device:nwk_s_key(Device0),
+                                                      router_device:app_s_key(Device0), I)},
+              timer:sleep(100)
+      end,
+      lists:seq(1, 9)),
+
+    lists:foreach(
+      fun(I) ->
+              %% Waiting for data from HTTP channel
+              test_utils:wait_channel_data(#{<<"id">> => ?CONSOLE_DEVICE_ID,
+                                             <<"name">> => ?CONSOLE_DEVICE_NAME,
+                                             <<"dev_eui">> => lorawan_utils:binary_to_hex(?DEVEUI),
+                                             <<"app_eui">> => lorawan_utils:binary_to_hex(?APPEUI),
+                                             <<"metadata">> => #{<<"labels">> => ?CONSOLE_LABELS},
+                                             <<"fcnt">> => I,
+                                             <<"reported_at">> => fun erlang:is_integer/1,
+                                             <<"payload">> => <<>>,
+                                             <<"port">> => 1,
+                                             <<"devaddr">> => '_',
+                                             <<"hotspots">> => [#{<<"id">> => erlang:list_to_binary(libp2p_crypto:bin_to_b58(PubKeyBin)),
+                                                                  <<"name">> => erlang:list_to_binary(HotspotName),
+                                                                  <<"reported_at">> => fun erlang:is_integer/1,
+                                                                  <<"status">> => <<"success">>,
+                                                                  <<"rssi">> => 0.0,
+                                                                  <<"snr">> => 0.0,
+                                                                  <<"spreading">> => <<"SF8BW125">>,
+                                                                  <<"frequency">> => fun erlang:is_float/1}]}),
+
+              %% Waiting for report channel status from HTTP channel
+              test_utils:wait_report_channel_status(#{<<"category">> => <<"up">>,
+                                                      <<"description">> => '_',
+                                                      <<"reported_at">> => fun erlang:is_integer/1,
+                                                      <<"device_id">> => ?CONSOLE_DEVICE_ID,
+                                                      <<"frame_up">> => fun erlang:is_integer/1,
+                                                      <<"frame_down">> => fun erlang:is_integer/1,
+                                                      <<"payload">> => <<>>, %% MAGIC Payload is here now...
+                                                      <<"payload_size">> => 0,
+                                                      <<"port">> => '_',
+                                                      <<"devaddr">> => '_',
+                                                      <<"hotspots">> => [#{<<"id">> => erlang:list_to_binary(libp2p_crypto:bin_to_b58(PubKeyBin)),
+                                                                           <<"name">> => erlang:list_to_binary(HotspotName),
+                                                                           <<"reported_at">> => fun erlang:is_integer/1,
+                                                                           <<"status">> => <<"success">>,
+                                                                           <<"rssi">> => 0.0,
+                                                                           <<"snr">> => 0.0,
+                                                                           <<"spreading">> => <<"SF8BW125">>,
+                                                                           <<"frequency">> => fun erlang:is_float/1}],
+                                                      <<"channels">> => [#{<<"id">> => ?CONSOLE_HTTP_CHANNEL_ID,
+                                                                           <<"name">> => ?CONSOLE_HTTP_CHANNEL_NAME,
+                                                                           <<"reported_at">> => fun erlang:is_integer/1,
+                                                                           <<"status">> => <<"success">>,
+                                                                           <<"description">> => '_',
+                                                                           <<"debug">> => #{<<"req">> =>
+                                                                                                #{<<"body">> => fun erlang:is_binary/1,
+                                                                                                  <<"headers">> => '_',
+                                                                                                  <<"method">> => <<"POST">>,
+                                                                                                  <<"url">> => <<?CONSOLE_URL/binary, "/channel">>},
+                                                                                            <<"res">> =>
+                                                                                                #{<<"body">> => <<"success">>,
+                                                                                                  <<"code">> => 200,
+                                                                                                  <<"headers">> => '_'}}}]})
+      end,
+      lists:seq(1, 9)),
+
+    State2 = sys:get_state(router_device_api_console),
+    ?assertMatch({state, _, _, _, #{}}, State2),
+
     ok.
 
 %% ------------------------------------------------------------------

173524cfb85b50992357f77aa8272e136f392552
macpie <pierre@helium.com>
2020-06-08 16:08:16 -0700

    Browan decoder can now handle port 204 also

--- src/decoders/router_decoder_browan_object_locator.erl
+++ src/decoders/router_decoder_browan_object_locator.erl
@@ -7,7 +7,8 @@ decode(_Decoder, << 0:1/integer, 0:1/integer, 0:1/integer, GNSError:1/integer, G
                     _:4/integer, Battery:4/unsigned-integer,
                     _:1/integer, Temp:7/unsigned-integer,
                     Lat:32/integer-signed-little,
-                    TempLon:24/integer-signed-little, Accuracy:3/integer, I:5/integer-unsigned>>, 136) ->
+                    TempLon:24/integer-signed-little, Accuracy:3/integer, I:5/integer-unsigned>>, Port) when Port == 136 orelse
+                                                                                                             Port == 204->
     <<Lon:29/integer-signed-little>> = <<TempLon:24/integer-unsigned-little, I:5/integer-unsigned>>,
     {ok, #{gns_error => GNSError == 1, gns_fix => GNSFix == 1, moving => Moving == 1, button => Button == 1, battery => (25 + Battery) / 10,
            temperature => Temp - 32, latitude => Lat / 1000000, longitude => Lon / 1000000, accuracy => trunc(math:pow(2, Accuracy+2))}};

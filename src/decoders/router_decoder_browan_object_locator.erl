fcabee5ad682a13c654656ec0890d2d67c4367d3
macpie <pierre@helium.com>
2020-06-08 16:26:45 -0700

    Add brown decoder for config state

--- src/decoders/router_decoder_browan_object_locator.erl
+++ src/decoders/router_decoder_browan_object_locator.erl
@@ -7,10 +7,15 @@ decode(_Decoder, << 0:1/integer, 0:1/integer, 0:1/integer, GNSError:1/integer, G
                     _:4/integer, Battery:4/unsigned-integer,
                     _:1/integer, Temp:7/unsigned-integer,
                     Lat:32/integer-signed-little,
-                    TempLon:24/integer-signed-little, Accuracy:3/integer, I:5/integer-unsigned>>, Port) when Port == 136 orelse
-                                                                                                             Port == 204->
+                    TempLon:24/integer-signed-little, Accuracy:3/integer, I:5/integer-unsigned>>, Port) when Port == 136 ->
     <<Lon:29/integer-signed-little>> = <<TempLon:24/integer-unsigned-little, I:5/integer-unsigned>>,
     {ok, #{gns_error => GNSError == 1, gns_fix => GNSFix == 1, moving => Moving == 1, button => Button == 1, battery => (25 + Battery) / 10,
            temperature => Temp - 32, latitude => Lat / 1000000, longitude => Lon / 1000000, accuracy => trunc(math:pow(2, Accuracy+2))}};
+decode(_Decoder, <<0:8/integer, UpdateIntervalWhileMoving:26/integer-unsigned-little, 1:8/integer,
+                   KeepAliveIntervalWhileStationary:16/integer-unsigned-little, 2:8/integer,
+                   GSensorTimeoutWhenMoving:16/integer-unsigned-little>>, Port) when Port == 204 ->
+    {ok, #{update_interval_moving => UpdateIntervalWhileMoving,
+           keepalive_interval_stationary => KeepAliveIntervalWhileStationary,
+           gsensor_timeout_moving => GSensorTimeoutWhenMoving}};
 decode(_Decoder, _, _) ->
     {error, browan_decoder_failure}.

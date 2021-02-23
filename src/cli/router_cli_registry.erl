19b0c488961febb2837f0a840b574907507b1883
macpie <pierre@helium.com>
2021-02-22 16:46:28 -0800

    Add snapshot CLI and router info CLI

--- src/cli/router_cli_registry.erl
+++ src/cli/router_cli_registry.erl
@@ -1,6 +1,6 @@
 -module(router_cli_registry).
 
--define(CLI_MODULES, [router_cli_dc_tracker, router_cli_device_worker]).
+-define(CLI_MODULES, [router_cli_dc_tracker, router_cli_device_worker, router_cli_info]).
 
 -export([register_cli/0]).
 

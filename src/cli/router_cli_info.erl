8ff6719af34631f91e30a0154d49c876194e6af4
macpie <macpie@users.noreply.github.com>
2021-02-23 09:00:29 -0800

    Add snapshot CLI and router info CLI (#222)

---
+++ src/cli/router_cli_info.erl
@@ -0,0 +1,85 @@
+%%%-------------------------------------------------------------------
+%% @doc router_cli_info
+%% @end
+%%%-------------------------------------------------------------------
+-module(router_cli_info).
+
+-behavior(clique_handler).
+
+-export([register_cli/0]).
+
+-define(USAGE, fun(_, _, _) -> usage end).
+
+-include("router_device_worker.hrl").
+
+register_cli() ->
+    register_all_usage(),
+    register_all_cmds().
+
+register_all_usage() ->
+    lists:foreach(
+        fun(Args) -> apply(clique, register_usage, Args) end,
+        [info_usage()]
+    ).
+
+register_all_cmds() ->
+    lists:foreach(
+        fun(Cmds) -> [apply(clique, register_command, Cmd) || Cmd <- Cmds] end,
+        [info_cmd()]
+    ).
+
+%%--------------------------------------------------------------------
+%% info
+%%--------------------------------------------------------------------
+
+info_usage() ->
+    [
+        ["info"],
+        [
+            "\n\n",
+            "info height        - Get height of the blockchain for this router\n",
+            "                       The first number is the current election epoch, and the second is\n"
+            "                       the block height.  If the second number is displayed with an asterisk (*)\n"
+            "                       this node has yet to sync past the assumed valid hash in the node config.\n\n"
+            "info name          - Get name for this router\n"
+        ]
+    ].
+
+info_cmd() ->
+    [
+        [["info", "height"], [], [], fun info_height/3],
+        [["info", "name"], [], [], fun info_name/3]
+    ].
+
+info_height(["info", "height"], [], []) ->
+    Chain = blockchain_worker:blockchain(),
+    {ok, Height} = blockchain:height(Chain),
+    {ok, SyncHeight} = blockchain:sync_height(Chain),
+    {ok, HeadBlock} = blockchain:head_block(Chain),
+    {Epoch0, _} = blockchain_block_v1:election_info(HeadBlock),
+    Epoch = integer_to_list(Epoch0),
+    case SyncHeight == Height of
+        true ->
+            [clique_status:text(Epoch ++ "\t\t" ++ integer_to_list(Height))];
+        false ->
+            [
+                clique_status:text([
+                    Epoch,
+                    "\t\t",
+                    integer_to_list(Height),
+                    "\t\t",
+                    integer_to_list(SyncHeight),
+                    "*"
+                ])
+            ]
+    end;
+info_height([_, _, _], [], []) ->
+    usage.
+
+info_name(["info", "name"], [], []) ->
+    {ok, Name} = erl_angry_purple_tiger:animal_name(
+        libp2p_crypto:bin_to_b58(blockchain_swarm:pubkey_bin())
+    ),
+    [clique_status:text(Name)];
+info_name([_, _, _], [], []) ->
+    usage.

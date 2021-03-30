6a3613636b443f199e1c88c0d0903f0c8ff0cac6
macpie <macpie@users.noreply.github.com>
2021-03-30 09:43:17 -0700

    Macpie/ch8558/router needs to provide a way to track requested (#242)
    * Decouple console api and ws
    
    * Send device's queue update to console (when asked)
    
    * Add max timer for 10 minutes for device queue update
    
    * Add CT for fetch queue
    
    * Fixup Ct after rebase
    
    * Upgrade meck

--- test/router_console_api_SUITE.erl
+++ test/router_console_api_SUITE.erl
@@ -6,13 +6,14 @@
     end_per_testcase/2
 ]).
 
--export([ws_get_address_test/1]).
+-export([ws_get_address_test/1, fetch_queue_test/1]).
 
 -include_lib("common_test/include/ct.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
 -include("utils/console_test.hrl").
 -include("lorawan_vars.hrl").
+-include("router_device_worker.hrl").
 
 -define(APPEUI, <<0, 0, 0, 2, 0, 0, 0, 1>>).
 -define(DEVEUI, <<0, 0, 0, 0, 0, 0, 0, 1>>).
@@ -28,7 +29,7 @@
 %% @end
 %%--------------------------------------------------------------------
 all() ->
-    [ws_get_address_test].
+    [ws_get_address_test, fetch_queue_test].
 
 %%--------------------------------------------------------------------
 %% TEST CASE SETUP
@@ -68,6 +69,111 @@ ws_get_address_test(_Config) ->
             )
     after 2500 -> ct:fail(websocket_msg_timeout)
     end,
+    ok.
+
+fetch_queue_test(Config) ->
+    _ = test_utils:join_device(Config),
+
+    %% Waiting for reply from router to hotspot
+    test_utils:wait_state_channel_message(1250),
+
+    %% Check that device is in cache now
+    {ok, DB, [_, CF]} = router_db:get(),
+    WorkerID = router_devices_sup:id(?CONSOLE_DEVICE_ID),
+    {ok, _Device0} = router_device:get_by_id(DB, CF, WorkerID),
+
+    %% Sending debug event from websocket
+    WSPid =
+        receive
+            {websocket_init, P} -> P
+        after 2500 -> ct:fail(websocket_init_timeout)
+        end,
+    WSPid ! device_fetch_queue,
+
+    receive
+        {websocket_msg, #{event := <<"router:address">>}} ->
+            ignore
+    after 500 -> ct:fail(websocket_router_address)
+    end,
+
+    receive
+        {websocket_msg, #{event := <<"device:all:downlink:update_queue">>, payload := Payload1}} ->
+            ?assertEqual(
+                #{
+                    <<"device">> => ?CONSOLE_DEVICE_ID,
+                    <<"queue">> => []
+                },
+                Payload1
+            )
+    after 500 -> ct:fail(websocket_update_queue)
+    end,
+
+    {ok, Pid} = router_devices_sup:lookup_device_worker(?CONSOLE_DEVICE_ID),
+
+    Channel = router_channel:new(
+        <<"channel_id">>,
+        random_handler,
+        <<"channel_name">>,
+        [],
+        ?CONSOLE_DEVICE_ID,
+        self()
+    ),
+    Downlink1 = #downlink{
+        confirmed = 1,
+        port = 0,
+        payload = <<"payload">>,
+        channel = Channel
+    },
+    ok = router_device_worker:queue_message(Pid, Downlink1),
+
+    receive
+        {websocket_msg, #{event := <<"device:all:downlink:update_queue">>, payload := Payload2}} ->
+            ?assertEqual(
+                #{
+                    <<"device">> => ?CONSOLE_DEVICE_ID,
+                    <<"queue">> => [
+                        #{
+                            <<"channel">> => #{
+                                <<"id">> => router_channel:id(Channel),
+                                <<"name">> => router_channel:name(Channel)
+                            },
+                            <<"confirmed">> => Downlink1#downlink.confirmed,
+                            <<"payload">> => Downlink1#downlink.payload,
+                            <<"port">> => Downlink1#downlink.port
+                        }
+                    ]
+                },
+                Payload2
+            )
+    after 500 -> ct:fail(websocket_update_queue)
+    end,
+
+    LabelID = <<"label_id">>,
+    WSPid ! {label_fetch_queue, LabelID},
+
+    receive
+        {websocket_msg, #{event := <<"label:all:downlink:update_queue">>, payload := Payload3}} ->
+            ?assertEqual(
+                #{
+                    <<"device">> => ?CONSOLE_DEVICE_ID,
+                    <<"label">> => <<"label_id">>,
+                    <<"queue">> => [
+                        #{
+                            <<"channel">> => #{
+                                <<"id">> => router_channel:id(Channel),
+                                <<"name">> => router_channel:name(Channel)
+                            },
+                            <<"confirmed">> => Downlink1#downlink.confirmed,
+                            <<"payload">> => Downlink1#downlink.payload,
+                            <<"port">> => Downlink1#downlink.port
+                        }
+                    ]
+                },
+                Payload3
+            )
+    after 500 -> ct:fail(websocket_update_queue)
+    end,
+
     ok.
 
 %% ------------------------------------------------------------------

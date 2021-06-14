aa2efac0bcff90de96db70b2dd97e3a70e360788
Michael Jeffrey <michaeldjeffrey@gmail.com>
2021-06-14 16:17:23 -0700

    Working get/create/delete

--- src/channels/router_azure_channel.erl
+++ src/channels/router_azure_channel.erl
@@ -19,6 +19,8 @@
     code_change/3
 ]).
 
+-define(CONNECTION_STRING, "HostName=pierre-iot-hub.azure-devices.net;SharedAccessKeyName=TestPolicy;SharedAccessKey=bMNBzrn+5BXtuRmZ6wv5NBS483IW2I/4Za9dDaVq7IE=").
+
 -record(state, {
     channel :: router_channel:channel()
 }).
@@ -98,6 +100,62 @@ get_device(Hostname, Token, DeviceID) ->
             error
     end.
 
+-spec create_device(binary(), binary(), binary(), map()) -> {ok, map()} | {error, any()}.
+create_device(HostName, Token, DesiredID, DeviceOptions) ->
+    URL = lists:flatten(
+            io_lib:format("https://~s/devices/~s?api-version=2020-03-13", [HostName, http_uri:encode(DesiredID)])
+           ),
+    Headers = [
+               {<<"Authorization">>, Token},
+               {<<"Content-Type">>, <<"application/json; charset=utf-8">>},
+               {<<"Accept">>, <<"application/json">>}
+              ],
+
+    Auth = #{
+             <<"type">> => <<"sas">>,
+             <<"symmetricKey">> => #{
+                                     <<"primaryKey">>  => <<"">>,
+                                     <<"secondaryKey">> => <<"">>
+                                    }
+            },
+
+    Payload = jsx:encode(maps:merge(DeviceOptions, #{<<"authentication">> =>Auth})),
+
+    lager:info("create_device ~p", [URL]),
+    case hackney:put(URL, Headers, Payload, [with_body, {ssl_options, [{versions, ['tlsv1.2']}]}]) of
+        {ok, 200, _Headers, Body} ->
+            Device = maps:from_list(jsx:decode(Body)),
+            {ok, Device};
+        Other ->
+            lager:warning("failed to create device ~p ~p", [DesiredID, Other]),
+            {error, Other}
+    end.
+
+-spec delete_device(binary(), binary(), binary(), binary() | force | explicit) -> {ok, map()} | {error, any()}.
+delete_device(HostName, Token, DeviceID, force) ->
+    delete_device(HostName, Token, DeviceID, <<"*">>);
+delete_device(HostName, Token, DeviceID, explicit) ->
+    {ok, #{<<"etag">> := ETag}} = get_device(HostName, Token, DeviceID),
+    delete_device(HostName, Token, DeviceID, ETag);
+delete_device(HostName, Token, DeviceID, ETag) ->
+    URL = lists:flatten(
+            io_lib:format("https://~s/devices/~s?api-version=2020-03-13", [HostName, http_uri:encode(DeviceID)])
+           ),
+    Headers = [
+               {<<"Authorization">>, Token},
+               {<<"Content-Type">>, <<"application/json; charset=utf-8">>},
+               {<<"Accept">>, <<"application/json">>},
+               {<<"If-Match">>, ETag}
+              ],
+
+    case hackney:delete(URL, Headers, <<>>, [with_body, {ssl_options, [{versions, ['tlsv1.2']}]}]) of
+        {ok, 204, _Headers, Body} ->
+            {ok, Body};
+        Other ->
+            lager:warning("failed to delete device ~p ~p", [DeviceID, Other]),
+            {error, Other}
+    end.
+
 %% ------------------------------------------------------------------
 %% EUNIT Tests
 %% ------------------------------------------------------------------
@@ -116,24 +174,112 @@ parse_connection_string_test() ->
     ok.
 
 get_device_test() ->
-    application:ensure_all_started(hackney),
     application:ensure_all_started(lager),
+    application:ensure_all_started(hackney),
 
-    Hostname = <<"XXX.azure-devices.net">>,
-    KeyName = <<"XXX">>,
-    Key = <<"XXXX">>,
-    Expires = 3600,
+    ConnString = ?CONNECTION_STRING,
+    {ok, HostName, Policy, Key} = parse_connection_string(ConnString),
+
     Token = generate_sas_token(
-        http_uri:encode(erlang:binary_to_list(Hostname)),
-        Key,
-        KeyName,
-        Expires
-    ),
+              http_uri:encode(erlang:binary_to_list(HostName)),
+              Key,
+              Policy,
+              _Expiry = 3600
+             ),
     DeviceID = <<"test-device-1">>,
-    {ok, Device} = get_device(Hostname, Token, DeviceID),
-    lager:notice("Device ~p", [Device]),
-    ?assert(false),
+
+    {ok, Device} = get_device(HostName, Token, DeviceID),
+    lager:info("Retrieved Device ~s", [io_lib:format("~n~120p~n", [Device])]),
+
     ok.
+
+create_device_test() ->
+    application:ensure_all_started(lager),
+    application:ensure_all_started(hackney),
+
+    ConnString = ?CONNECTION_STRING,
+    {ok, HostName, Policy, Key} = parse_connection_string(ConnString),
+
+
+    Token = generate_sas_token(
+              http_uri:encode(erlang:binary_to_list(HostName)),
+              Key,
+              Policy,
+              _Expiry = 3600
+             ),
+
+    DeviceForceName = <<"test-device-force">>,
+    DeviceForceOptions = #{<<"deviceId">> => DeviceForceName},
+    DeviceExplicitName = <<"test-device-explicit">>,
+    DeviceExplicitOptions = #{<<"deviceID">> => DeviceExplicitName},
+
+    {ok, DeviceForce} = create_device(HostName, Token, DeviceForceName, DeviceForceOptions),
+    lager:info("Created Device ~s", [io_lib:format("~n~120p~n", [DeviceForce])]),
+
+    {ok, DeviceExplicit} = create_device(HostName, Token, DeviceExplicitName, DeviceExplicitOptions),
+    lager:info("Created Device ~s", [io_lib:format("~n~120p~n", [DeviceExplicit])]),
+
+    ok.
+
+delete_device_force_test() ->
+    application:ensure_all_started(lager),
+    application:ensure_all_started(hackney),
+
+    ConnString = ?CONNECTION_STRING,
+    {ok, HostName, Policy, Key} = parse_connection_string(ConnString),
+
+    DeviceID = <<"test-device-force">>,
+    Token = generate_sas_token(
+              http_uri:encode(erlang:binary_to_list(HostName)),
+              Key,
+              Policy,
+              _Expiry = 3600
+             ),
+
+    {ok, Device} = delete_device(HostName, Token, DeviceID, force),
+    lager:info("Deleted Device ~p", [Device]),
+
+    ok.
+
+delete_device_explicit_test() ->
+    application:ensure_all_started(lager),
+    application:ensure_all_started(hackney),
+
+    ConnString = ?CONNECTION_STRING,
+    {ok, HostName, Policy, Key} = parse_connection_string(ConnString),
+
+    DeviceID = <<"test-device-explicit">>,
+    Token = generate_sas_token(
+              http_uri:encode(erlang:binary_to_list(HostName)),
+              Key,
+              Policy,
+              _Expiry = 3600
+             ),
+
+    {ok, Device} = delete_device(HostName, Token, DeviceID, force),
+    lager:info("Deleted Device ~p", [Device]),
+
+    ok.
+
+%% get_device_test() ->
+%%     application:ensure_all_started(hackney),
+%%     application:ensure_all_started(lager),
+
+%%     Hostname = <<"pierre-iot-hub.azure-devices.net">>,
+%%     KeyName = <<"iothubowner">>,
+%%     Key = <<"XXXX">>,
+%%     Expires = 3600,
+%%     Token = generate_sas_token(
+%%         http_uri:encode(erlang:binary_to_list(Hostname)),
+%%         Key,
+%%         KeyName,
+%%         Expires
+%%     ),
+%%     DeviceID = <<"test-device-1">>,
+%%     {ok, Device} = get_device(Hostname, Token, DeviceID),
+%%     lager:notice("Device ~p", [Device]),
+%%     ?assert(false),
+%%     ok.
 
 generate_sas_token(URI, Key, PolicyName, Expires) ->
     ExpireString = erlang:integer_to_list(erlang:system_time(seconds) + Expires),
@@ -154,98 +300,98 @@ generate_sas_token(URI, Key, PolicyName, Expires) ->
             SAS ++ "&skn=" ++ http_uri:encode(erlang:binary_to_list(PolicyName))
     end.
 
-create_device_test() ->
-    application:ensure_all_started(hackney),
-    application:ensure_all_started(lager),
+%% create_device_test() ->
+%%     application:ensure_all_started(hackney),
+%%     application:ensure_all_started(lager),
 
-    Hostname = <<"XXX.azure-devices.net">>,
-    KeyName = <<"XXX">>,
-    Key = <<"XXXX">>,
-    Expires = 3600,
-    Token = generate_sas_token(
-        http_uri:encode(erlang:binary_to_list(Hostname)),
-        Key,
-        KeyName,
-        Expires
-    ),
-    DeviceID = <<"test-device-1">>,
+%%     Hostname = <<"XXX.azure-devices.net">>,
+%%     KeyName = <<"XXX">>,
+%%     Key = <<"XXXX">>,
+%%     Expires = 3600,
+%%     Token = generate_sas_token(
+%%         http_uri:encode(erlang:binary_to_list(Hostname)),
+%%         Key,
+%%         KeyName,
+%%         Expires
+%%     ),
+%%     DeviceID = <<"test-device-1">>,
 
-    ok.
+%%     ok.
 
-create_certificate() ->
-    <<SerialNumber:128/integer-unsigned>> = crypto:strong_rand_bytes(16),
-    Rdn =
-        {rdnSequence, [
-            #'AttributeTypeAndValue'{
-                type = ?'id-at-countryName',
-                value = pack_country(<<"US">>)
-            },
-            #'AttributeTypeAndValue'{
-                type = ?'id-at-stateOrProvinceName',
-                value = pack_string(<<"California">>)
-            },
-            #'AttributeTypeAndValue'{
-                type = ?'id-at-localityName',
-                value = pack_string(<<"San Francisco">>)
-            },
-            #'AttributeTypeAndValue'{
-                type = ?'id-at-organizationName',
-                value = pack_string(<<"Helium">>)
-            },
-            #'AttributeTypeAndValue'{
-                type = ?'id-at-commonName',
-                value = pack_string(<<"Device id and hotsname here?">>)
-            }
-        ]},
-    TBSCertificate = #'TBSCertificate'{
-        version = 0,
-        serialNumber = SerialNumber,
-        signature = #'AlgorithmIdentifier'{
-            algorithm = ?'ecdsa-with-SHA256',
-            parameters = asn1_NOVALUE
-        },
-        issuer = Rdn,
-        validity = validity(30 * 12 * 10),
-        subject = Rdn,
-        subjectPublicKeyInfo = #'SubjectPublicKeyInfo'{
-            algorithm = #'AlgorithmIdentifier'{
-                algorithm = ?'id-ecPublicKey',
-                parameters = <<6, 8, 42, 134, 72, 206, 61, 3, 1, 7>>
-            },
-            subjectPublicKey = PublicKey
-        },
-        issuerUniqueID = asn1_NOVALUE,
-        subjectUniqueID = asn1_NOVALUE,
-        extensions = asn1_NOVALUE
-    },
-    DER = public_key:der_encode('TBSCertificate', TBSCertificate),
-    Signature = public_key:sign(DER, sha256, PrivateKey),
-    #'Certificate'{
-        tbsCertificate = TBSCertificate,
-        signatureAlgorithm = #'AlgorithmIdentifier'{
-            algorithm = ?md5WithRSAEncryption,
-            parameters = <<5, 0>>
-        },
-        signature = {0, Signature}
-    }.
+%% create_certificate() ->
+%%     <<SerialNumber:128/integer-unsigned>> = crypto:strong_rand_bytes(16),
+%%     Rdn =
+%%         {rdnSequence, [
+%%             #'AttributeTypeAndValue'{
+%%                 type = ?'id-at-countryName',
+%%                 value = pack_country(<<"US">>)
+%%             },
+%%             #'AttributeTypeAndValue'{
+%%                 type = ?'id-at-stateOrProvinceName',
+%%                 value = pack_string(<<"California">>)
+%%             },
+%%             #'AttributeTypeAndValue'{
+%%                 type = ?'id-at-localityName',
+%%                 value = pack_string(<<"San Francisco">>)
+%%             },
+%%             #'AttributeTypeAndValue'{
+%%                 type = ?'id-at-organizationName',
+%%                 value = pack_string(<<"Helium">>)
+%%             },
+%%             #'AttributeTypeAndValue'{
+%%                 type = ?'id-at-commonName',
+%%                 value = pack_string(<<"Device id and hotsname here?">>)
+%%             }
+%%         ]},
+%%     TBSCertificate = #'TBSCertificate'{
+%%         version = 0,
+%%         serialNumber = SerialNumber,
+%%         signature = #'AlgorithmIdentifier'{
+%%             algorithm = ?'ecdsa-with-SHA256',
+%%             parameters = asn1_NOVALUE
+%%         },
+%%         issuer = Rdn,
+%%         validity = validity(30 * 12 * 10),
+%%         subject = Rdn,
+%%         subjectPublicKeyInfo = #'SubjectPublicKeyInfo'{
+%%             algorithm = #'AlgorithmIdentifier'{
+%%                 algorithm = ?'id-ecPublicKey',
+%%                 parameters = <<6, 8, 42, 134, 72, 206, 61, 3, 1, 7>>
+%%             },
+%%             subjectPublicKey = PublicKey
+%%         },
+%%         issuerUniqueID = asn1_NOVALUE,
+%%         subjectUniqueID = asn1_NOVALUE,
+%%         extensions = asn1_NOVALUE
+%%     },
+%%     DER = public_key:der_encode('TBSCertificate', TBSCertificate),
+%%     Signature = public_key:sign(DER, sha256, PrivateKey),
+%%     #'Certificate'{
+%%         tbsCertificate = TBSCertificate,
+%%         signatureAlgorithm = #'AlgorithmIdentifier'{
+%%             algorithm = ?md5WithRSAEncryption,
+%%             parameters = <<5, 0>>
+%%         },
+%%         signature = {0, Signature}
+%%     }.
 
-pack_country(Bin) when size(Bin) == 2 ->
-    <<19, 2, Bin:2/binary>>.
+%% pack_country(Bin) when size(Bin) == 2 ->
+%%     <<19, 2, Bin:2/binary>>.
 
-pack_string(Bin) ->
-    Size = byte_size(Bin),
-    <<12, Size:8/integer-unsigned, Bin/binary>>.
+%% pack_string(Bin) ->
+%%     Size = byte_size(Bin),
+%%     <<12, Size:8/integer-unsigned, Bin/binary>>.
 
-validity(Days) ->
-    Now = calendar:universal_time(),
-    Start = minute_before(Now),
-    {Date, Time} = Start,
-    StartDays = calendar:date_to_gregorian_days(Date),
-    EndDays = StartDays + Days,
-    End = {calendar:gregorian_days_to_date(EndDays), Time},
-    #'Validity'{
-        notBefore = datetime_to_utc_time(Start),
-        notAfter = datetime_to_utc_time(End)
-    }.
+%% validity(Days) ->
+%%     Now = calendar:universal_time(),
+%%     Start = minute_before(Now),
+%%     {Date, Time} = Start,
+%%     StartDays = calendar:date_to_gregorian_days(Date),
+%%     EndDays = StartDays + Days,
+%%     End = {calendar:gregorian_days_to_date(EndDays), Time},
+%%     #'Validity'{
+%%         notBefore = datetime_to_utc_time(Start),
+%%         notAfter = datetime_to_utc_time(End)
+%%     }.
 
 -endif.

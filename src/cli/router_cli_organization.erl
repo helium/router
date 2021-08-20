089aebde63fe5b32911c6a671f8434941d17b315
macpie <pierre@helium.com>
2021-08-20 11:37:03 -0700

    Add single org fetch Add --more flag

--- src/cli/router_cli_organization.erl
+++ src/cli/router_cli_organization.erl
@@ -39,7 +39,8 @@ org_usage() ->
         ["organization"],
         [
             "Organization commands\n\n",
-            "  info all [--less 42] - Display DC for all Orgs\n"
+            "  info all [--less 42] [--more 42]  - Display DC for all Orgs\n",
+            "  info <org_id>                     - Display info for single Org\n"
         ]
     ].
 
@@ -57,7 +58,8 @@ org_info_usage() ->
         ["organization", "info"],
         [
             "Info commands\n\n",
-            "  info all [--less 42]   - Display DC for all Orgs\n"
+            "  info all [--less 42] [--more 42]    - Display DC for all Orgs\n",
+            "  info <org_id>                       - Display info for single Org\n"
         ]
     ].
 
@@ -70,43 +72,64 @@ org_info_cmd() ->
                 {less_than, [
                     {longname, "less"},
                     {datatype, integer}
+                ]},
+                {more_than, [
+                    {longname, "more"},
+                    {datatype, integer}
                 ]}
             ],
             fun org_info_all/3
-        ]
+        ],
+        [["organization", "info", '*'], [], [], fun org_info_single/3]
     ].
 
 org_info_all(["organization", "info", "all"], [], Flags) ->
-    APIOrgs = api_orgs(),
+    APIOrgs = get_api_orgs(),
     case APIOrgs of
         [] ->
             c_text("No organization found.");
         _ ->
-            DCTracketOrgs = dc_tracker_orgs(),
-            LessThan = proplists:get_value(less_than, Flags),
+            DCTracketOrgs = get_dc_tracker_orgs(),
+            LessThan = proplists:get_value(less_than, Flags, undefined),
+            MoreThan = proplists:get_value(more_than, Flags, undefined),
             OrgList = lists:filtermap(
-                fun(Map) ->
-                    Balance = maps:get(balance, Map),
-                    case Balance < LessThan of
-                        false ->
+                fun(Org) ->
+                    ID = proplists:get_value(id, Org),
+                    {CacheBalance, CacheNonce} =
+                        case proplists:get_value(ID, DCTracketOrgs) of
+                            undefined ->
+                                {undefined, undefined};
+                            {B, N} ->
+                                {B, N}
+                        end,
+                    True =
+                        {true,
+                            Org ++
+                                [
+                                    {cached_balance, CacheBalance},
+                                    {cached_nonce, CacheNonce}
+                                ]},
+                    Balance = proplists:get_value(balance, Org),
+                    FilterBalance =
+                        case Balance of
+                            undefined -> 0;
+                            _ -> Balance
+                        end,
+                    case {LessThan, MoreThan} of
+                        {undefined, undefined} ->
+                            True;
+                        {undefined, _} when FilterBalance > MoreThan ->
+                            True;
+                        {undefined, _} ->
                             false;
-                        _ ->
-                            ID = maps:get(id, Map),
-                            {CacheBalance, CacheNonce} =
-                                case proplists:get_value(ID, DCTracketOrgs) of
-                                    undefined ->
-                                        {undefined, undefined};
-                                    {B, N} ->
-                                        {B, N}
-                                end,
-                            {true, [
-                                {id, ID},
-                                {name, maps:get(name, Map)},
-                                {balance, Balance},
-                                {nonce, maps:get(nonce, Map)},
-                                {cached_balance, CacheBalance},
-                                {cached_nonce, CacheNonce}
-                            ]}
+                        {_, undefined} when FilterBalance < LessThan ->
+                            True;
+                        {_, undefined} ->
+                            false;
+                        {_, _} when FilterBalance < LessThan andalso FilterBalance > MoreThan ->
+                            True;
+                        {_, _} ->
+                            false
                     end
                 end,
                 APIOrgs
@@ -119,19 +142,59 @@ org_info_all(["organization", "info", "all"], [], Flags) ->
             end
     end.
 
+org_info_single(["organization", "info", OrgStr], [], _Flags) ->
+    OrgID = erlang:list_to_binary(OrgStr),
+    case router_console_api:get_org(OrgID) of
+        {error, _} ->
+            c_text("Organization not found.");
+        {ok, Org} ->
+            {CacheBalance, CacheNonce} =
+                case router_console_dc_tracker:lookup(OrgID) of
+                    {ok, B, N} ->
+                        {B, N};
+                    {error, _} ->
+                        {undefined, undefined}
+                end,
+            c_table([
+                format_api_org(Org) ++
+                    [
+                        {cached_balance, CacheBalance},
+                        {cached_nonce, CacheNonce}
+                    ]
+            ])
+    end.
+
 %%--------------------------------------------------------------------
 %% Private Utilities
 %%--------------------------------------------------------------------
 
--spec dc_tracker_orgs() -> list({binary(), balance_nonce()}).
-dc_tracker_orgs() ->
+-spec get_dc_tracker_orgs() -> list({binary(), balance_nonce()}).
+get_dc_tracker_orgs() ->
     router_console_dc_tracker:lookup_all().
 
--spec api_orgs() -> list(map()).
-api_orgs() ->
-    [#{id => <<"test">>, name => <<"Test Org">>, balance => 10, nonce => 1}].
+-spec get_api_orgs() -> list(list()).
+get_api_orgs() ->
+    case router_console_api:get_orgs() of
+        {error, _} ->
+            [];
+        {ok, Orgs} ->
+            [format_api_org(Org) || Org <- Orgs]
+    end.
 
--spec c_table(proplists:proplist()) -> clique_status:status().
+-spec format_api_org(map()) -> list().
+format_api_org(Map) ->
+    [
+        {id, not_null(maps:get(<<"id">>, Map, undefined))},
+        {name, not_null(maps:get(<<"name">>, Map, undefined))},
+        {balance, not_null(maps:get(<<"dc_balance">>, Map, undefined))},
+        {nonce, not_null(maps:get(<<"dc_balance_nonce">>, Map, undefined))}
+    ].
+
+-spec not_null(any()) -> any().
+not_null(null) -> undefined;
+not_null(Any) -> Any.
+
+-spec c_table(list()) -> clique_status:status().
 c_table(PropLists) ->
     [clique_status:table(PropLists)].
 

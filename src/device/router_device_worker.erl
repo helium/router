7f6ec7a9344266dbeeae358bb6923f3b5e335dcc
macpie <macpie@users.noreply.github.com>
2021-11-22 12:15:36 -0800

    Update src/device/router_device_worker.erl
    Co-authored-by: Michael Jeffrey <michaeldjeffrey@gmail.com>

--- src/device/router_device_worker.erl
+++ src/device/router_device_worker.erl
@@ -1340,7 +1340,7 @@ do_multi_buy(Packet, Device, FrameAck) ->
 ) ->
     {error, any()}
     | {ok, #frame{}, router_device:device(), SendToChannel :: boolean(),
-        {Balance :: non_neg_integer(), Nonce :: non_neg_integer()}, boolean()}.
+        {Balance :: non_neg_integer(), Nonce :: non_neg_integer()}, Replay :: boolean()}.
 validate_frame(
     Packet,
     PacketTime,

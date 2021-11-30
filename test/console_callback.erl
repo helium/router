1f5405f6039c26adfee0bf61239587761e631199
Michael Jeffrey <michaeldjeffrey@gmail.com>
2021-11-29 17:35:00 -0700

    After exhausting console devices, put them back
    Some tests (xor in particular) rely on being to treat Console endpoints
    like a stable store. When we're paginating the results put all the
    devices back so we can do it again.
    
    There could still be race conditions if the tests are ever parallelized.

--- test/console_callback.erl
+++ test/console_callback.erl
@@ -76,21 +76,23 @@ handle('GET', [<<"api">>, <<"router">>, <<"devices">>], Req, Args) ->
                         }
                     ],
                     undefined};
-            [{devices, {Qs, []}}] ->
-                true = ets:delete(Tab, devices),
+            [{devices, {Qs, [], Refill}}] ->
+                true = ets:insert(Tab, {devices, Refill}),
                 {[], undefined};
-            [{devices, {Qs, Ds}}] ->
+            [{devices, {Qs, Ds, Refill}}] ->
                 UUID = router_utils:uuid_v4(),
                 {PickedDevices, LeftOverDevices} = split_list(Ds),
-                true = ets:insert(Tab, {devices, {<<"after=", UUID/binary>>, LeftOverDevices}}),
+                true = ets:insert(
+                    Tab,
+                    {devices, {<<"after=", UUID/binary>>, LeftOverDevices, Refill}}
+                ),
                 {lists:map(fun device_to_json/1, PickedDevices), UUID};
             [{devices, Ds}] ->
                 UUID = router_utils:uuid_v4(),
                 {PickedDevices, LeftOverDevices} = split_list(Ds),
-                true = ets:insert(Tab, {devices, {<<"after=", UUID/binary>>, LeftOverDevices}}),
+                true = ets:insert(Tab, {devices, {<<"after=", UUID/binary>>, LeftOverDevices, Ds}}),
                 {lists:map(fun device_to_json/1, PickedDevices), UUID}
         end,
-
     case ResourceID of
         undefined ->
             {200, [], jsx:encode(#{data => Devices})};

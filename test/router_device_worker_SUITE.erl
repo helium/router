ddd90ff059fd4bd8deb63bfd7f62adafe4c71ab3
mikev <mvierling@gmail.com>
2022-06-07 13:22:27 -0700

    update test for link_adr_req

--- test/router_device_worker_SUITE.erl
+++ test/router_device_worker_SUITE.erl
@@ -287,17 +287,17 @@ device_worker_late_packet_double_charge_test(Config) ->
                     <<"channel_mask">> => 0,
                     <<"channel_mask_control">> => 7,
                     <<"command">> => <<"link_adr_req">>,
-                    <<"data_rate">> => 15,
+                    <<"data_rate">> => 2,
                     <<"number_of_transmissions">> => 0,
-                    <<"tx_power">> => 15
+                    <<"tx_power">> => 0
                 },
                 #{
                     <<"channel_mask">> => 65280,
                     <<"channel_mask_control">> => 0,
                     <<"command">> => <<"link_adr_req">>,
-                    <<"data_rate">> => 15,
+                    <<"data_rate">> => 2,
                     <<"number_of_transmissions">> => 0,
-                    <<"tx_power">> => 15
+                    <<"tx_power">> => 0
                 }
             ]
         }

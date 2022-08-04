833506c36070e8f057ae51f04821e86be58fe40d
Michael Jeffrey <michaeldjeffrey@gmail.com>
2022-08-03 17:20:44 -0700

    Check if this is the first packet we've ever seen
    `LastSeenFCnt` can be undefined if the device just joined, and thanks to
    total ordering any frame count will always be less than `undefined`. So
    we make a new case right at the beginning for it.

--- src/device/router_device_worker.erl
+++ src/device/router_device_worker.erl
@@ -1442,7 +1442,7 @@ craft_join_reply(
     Region :: atom(),
     Device :: router_device:device(),
     Blockchain :: blockchain:blockchain(),
-    {LastSeenFCnt :: non_neg_integer(), DownlinkHanldedAt :: {integer(), integer()}},
+    {LastSeenFCnt :: undefined | non_neg_integer(), DownlinkHanldedAt :: {integer(), integer()}},
     FrameCache :: #{integer() => #frame_cache{}},
     OfferCache :: map()
 ) ->
@@ -1480,6 +1480,18 @@ validate_frame(
                         Blockchain,
                         false
                     );
+                undefined when FrameAck == 0 andalso LastSeenFCnt == undefined ->
+                    lager:debug("we got a first packet [fcnt: ~p]", [PacketFCnt]),
+                    validate_frame_(
+                        PacketFCnt,
+                        Packet,
+                        PubKeyBin,
+                        Region,
+                        Device0,
+                        OfferCache,
+                        Blockchain,
+                        false
+                    );
                 undefined when FrameAck == 0 andalso PacketFCnt =< LastSeenFCnt ->
                     lager:debug("we got a late unconfirmed up packet for ~p: LastSeenFCnt: ~p", [
                         PacketFCnt,
@@ -1507,6 +1519,7 @@ validate_frame(
                         PacketFCnt, Packet, PubKeyBin, Region, Device0, OfferCache, Blockchain, true
                     );
                 undefined ->
+                    lager:debug("we got a fresh packet [fcnt: ~p]", [PacketFCnt]),
                     validate_frame_(
                         PacketFCnt,
                         Packet,

ab341a958e76a3de4c284a2d9604d66b6c7c1956
Macpie <pierre@nova-labs.com>
2023-01-27 12:35:17 -0800

    Finish router_ics_worker_SUITE

--- test/router_ics_worker_SUITE.erl
+++ test/router_ics_worker_SUITE.erl
@@ -127,40 +127,48 @@ main_test(_Config) ->
         Req3#iot_config_route_update_euis_req_v1_pb.eui_pair
     ),
 
-    % ok = router_ics_worker:remove([ID2]),
+    ok = router_ics_worker:remove([ID2]),
 
-    % [{Type3, Req3}] = rcv_loop([]),
-    % ?assertEqual(euis, Type3),
-    % ?assertEqual(remove_euis, Req3#route_euis_req_v1_pb.action),
-    % ?assertEqual([#eui_v1_pb{app_eui = 1, dev_eui = 2}], Req3#route_euis_req_v1_pb.euis),
+    [{Type4, Req4}] = rcv_loop([]),
+    ?assertEqual(update_euis, Type4),
+    ?assertEqual(remove, Req4#iot_config_route_update_euis_req_v1_pb.action),
+    ?assertEqual(
+        #iot_config_eui_pair_v1_pb{route_id = RouteID, app_eui = 1, dev_eui = 2},
+        Req4#iot_config_route_update_euis_req_v1_pb.eui_pair
+    ),
 
-    % meck:expect(router_console_api, get_device, fun(DeviceID) ->
-    %     lager:notice("router_console_api:get_device(~p)", [DeviceID]),
-    %     case DeviceID of
-    %         ID1 -> {error, not_found};
-    %         ID2 -> {ok, maps:get(ID2, Devices)}
-    %     end
-    % end),
+    meck:expect(router_console_api, get_device, fun(DeviceID) ->
+        lager:notice("router_console_api:get_device(~p)", [DeviceID]),
+        case DeviceID of
+            ID1 -> {error, not_found};
+            ID2 -> {ok, maps:get(ID2, Devices)}
+        end
+    end),
 
-    % meck:expect(router_device_cache, get, fun(DeviceID) ->
-    %     lager:notice("router_console_api:get_device(~p)", [DeviceID]),
-    %     case DeviceID of
-    %         ID1 -> {ok, maps:get(ID1, Devices)};
-    %         ID2 -> {error, not_found}
-    %     end
-    % end),
+    meck:expect(router_device_cache, get, fun(DeviceID) ->
+        lager:notice("router_console_api:get_device(~p)", [DeviceID]),
+        case DeviceID of
+            ID1 -> {ok, maps:get(ID1, Devices)};
+            ID2 -> {error, not_found}
+        end
+    end),
 
-    % ok = router_ics_worker:update([ID1, ID2]),
+    ok = router_ics_worker:update([ID1, ID2]),
 
-    % [{Type5, Req5}, {Type4, Req4}] = rcv_loop([]),
-    % ?assertEqual(euis, Type5),
-    % ?assertEqual(add_euis, Req5#route_euis_req_v1_pb.action),
-    % ?assertEqual([#eui_v1_pb{app_eui = 1, dev_eui = 2}], Req5#route_euis_req_v1_pb.euis),
-    % ?assertEqual(euis, Type4),
-    % ?assertEqual(remove_euis, Req4#route_euis_req_v1_pb.action),
-    % ?assertEqual([#eui_v1_pb{app_eui = 1, dev_eui = 1}], Req4#route_euis_req_v1_pb.euis),
+    [{Type6, Req6}, {Type5, Req5}] = rcv_loop([]),
+    ?assertEqual(update_euis, Type5),
+    ?assertEqual(remove, Req5#iot_config_route_update_euis_req_v1_pb.action),
+    ?assertEqual(
+        #iot_config_eui_pair_v1_pb{route_id = RouteID, app_eui = 1, dev_eui = 1},
+        Req5#iot_config_route_update_euis_req_v1_pb.eui_pair
+    ),
 
-    timer:sleep(5000),
+    ?assertEqual(update_euis, Type6),
+    ?assertEqual(add, Req6#iot_config_route_update_euis_req_v1_pb.action),
+    ?assertEqual(
+        #iot_config_eui_pair_v1_pb{route_id = RouteID, app_eui = 1, dev_eui = 2},
+        Req6#iot_config_route_update_euis_req_v1_pb.eui_pair
+    ),
 
     meck:unload(router_console_api),
     meck:unload(router_device_cache),

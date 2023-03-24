9e663cadf189c972ebed7c41cb9eab04e7ffd6ad
Michael Jeffrey <michaeldjeffrey@gmail.com>
2023-03-24 13:36:21 -0700

    opt for merge so configuration takes precendent

--- src/router_sup.erl
+++ src/router_sup.erl
@@ -133,7 +133,6 @@ init([]) ->
     PubKeyBin = libp2p_crypto:pubkey_to_bin(PubKey0),
     ICSOptsDefault = application:get_env(router, ics, #{}),
     ICSOpts = maps:merge(#{pubkey_bin => PubKeyBin, sig_fun => SigFun}, ICSOptsDefault),
-    %% ICSOpts = ICSOptsDefault#{pubkey_bin => PubKeyBin, sig_fun => SigFun},
 
     router_ics_gateway_location_worker:init_ets(),
 
